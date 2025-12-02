690c2568f5e7ea63471b94fc04d4a9508329b0ce
Gustavo Santos <gustavogomes.bsb68@gmail.com>
2025-12-02 10:17:45 -0300

    Professor

--- src/main/java/br/edu/uniesp/softfact/application/professor/ProfessorUpdateRequest.java
+++ src/main/java/br/edu/uniesp/softfact/application/professor/ProfessorUpdateRequest.java
@@ -1,15 +1,12 @@
 package br.edu.uniesp.softfact.application.professor;
 
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
-public record ProfessorCreateRequest(
-        @NotBlank(message = "O nome é obrigatório.")
+public record ProfessorUpdateRequest(
         @Size(max = 150)
         String nome,
 
-        @NotBlank(message = "O email é obrigatório.")
         @Email(message = "Formato de email inválido.")
         @Size(max = 150)
         String email
